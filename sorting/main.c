04288e4ed5406bacc0d9ed3e11045f116509212a
Max Mendez <84734574+maxmxyz2003@users.noreply.github.com>
2026-08-18 12:34:06 -0600

    Add Bubble Sort function
    Implemented Bubble Sort algorithm in main.c.

--- sorting/main.c
+++ sorting/main.c
@@ -1,6 +1,18 @@
 // main.c
 #include <stdio.h>
 #include "mymath.h"
+void BubbleSort(int arr[], int size) {
+  int temp;
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
 
 int main() {
     return 0;
