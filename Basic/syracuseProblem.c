63c571cd2990ba5351f8e598335c8beecca9b330
Max Mendez <84734574+maxmxyz2003@users.noreply.github.com>
2026-09-07 13:04:12 -0600

    Implement recursive function for Syracuse problem
    Added recursive function 'syr' to handle Syracuse problem.

--- Basic/syracuseProblem.c
+++ Basic/syracuseProblem.c
@@ -18,7 +18,22 @@ void tres_n_mas_uno(long int numero, long int *cont, long int *mayor) {
     }
   }
 }
-
+void syr(int num){ // recursive 
+    if(num==1){
+        printf("1");  
+    }else if (num%2==0)
+    {
+        syr(num*3+1);
+        printf("%d \n", num);
+    }else if (num%2==1){
+        syr(num/2);
+        printf("%d \n", num);
+    }    
+}
+void cambia(const int a[]){
+    int nuevo=a[1];
+    
+}
 int main(void) {
   long int elcontad = 0, numero, elmayor = 1;
   scanf("%ld", &numero);
