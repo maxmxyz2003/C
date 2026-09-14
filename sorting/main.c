e3a933d9ae4ccfcd9c688943ae8abe37a146ba4b
Max Mendez <84734574+maxmxyz2003@users.noreply.github.com>
2026-09-14 11:43:47 -0600

    Add merge sort implementation and print function
    Implemented merge sort algorithm and added print function for arrays.

--- sorting/main.c
+++ sorting/main.c
@@ -45,10 +45,66 @@ void DisplayArray(long int arr[], int n){
     }
 }
 
+// MERGESORT Complexity O(nlog(n))
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[n1], R[n2];
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+void printArray(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
 int main(void){
     long int arr[]={5,4,7,88,32,43,11,15};
     DisplayArray(arr,sizeof(arr)/sizeof(arr[0]));
     printf("\n");
     QuickSort_Asc(arr,0,sizeof(arr)/sizeof(arr[0])-1);
     DisplayArray(arr,sizeof(arr)/sizeof(arr[0]));
+   //----
+  int arr[] = {12, 11, 13, 5, 6, 7};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+    printf("Arreglo original:\n");
+    printArray(arr, arr_size);
+    mergeSort(arr, 0, arr_size - 1);
+    printf("Arreglo ordenado:\n");
+    printArray(arr, arr_size);
 }
