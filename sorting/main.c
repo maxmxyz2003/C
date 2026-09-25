9f1ec4a28e4d2d7e688141eaf8fee70ebbf4c685
Max Mendez <84734574+maxmxyz2003@users.noreply.github.com>
2026-09-25 11:53:19 -0600

    Add Radix Sort implementation in main.c
    Implemented Radix Sort algorithm and added function to find maximum element.

--- sorting/main.c
+++ sorting/main.c
@@ -93,6 +93,40 @@ void printArray(int arr[], int size) {
         printf("%d ", arr[i]);
     printf("\n");
 }
+int encontrarMaximo(int arr[], int n) {
+    int maximo = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > maximo) {
+            maximo = arr[i];
+        }
+    }
+    return maximo;
+}
+void RadixSort(int arr[], int n) {
+    int maximo = encontrarMaximo(arr, n);// Realizar el conteo de frecuencia de cada dígito
+    for (int exp = 1; maximo / exp > 0; exp *= 10) {
+        int conteo[10] = {0}; // Inicializar el arreglo de conteo a 0
+        // Contar la frecuencia de cada dígito en la posición 'exp'
+        for (int i = 0; i < n; i++) {
+            conteo[(arr[i] / exp) % 10]++;
+        }
+        // Actualizar el conteo para indicar la posición real de los dígitos en el arreglo ordenado
+        for (int i = 1; i < 10; i++) {
+            conteo[i] += conteo[i - 1];
+        }
+        // Construir el arreglo ordenado
+        int salida[n];
+        for (int i = n - 1; i >= 0; i--) {
+            salida[conteo[(arr[i] / exp) % 10] - 1] = arr[i];
+            conteo[(arr[i] / exp) % 10]--;
+        }
+        // Copiar el arreglo ordenado de salida al arreglo original
+        for (int i = 0; i < n; i++) {
+            arr[i] = salida[i];
+        }
+    }
+}
+
 int main(void){
     long int arr[]={5,4,7,88,32,43,11,15};
     DisplayArray(arr,sizeof(arr)/sizeof(arr[0]));
